09f453a3b6f7ad4150be914683b7037c7da7515a
ismyl <ismyl.awn@gmail.com>
2020-06-23 14:04:53 +0900

    - Difference Of Squares Solution

---
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,16 @@
+import java.util.stream.IntStream;
+
+class DifferenceOfSquaresCalculator {
+
+  int computeSquareOfSumTo(int input) {
+    return (int) Math.pow(IntStream.rangeClosed(1, input).sum(), 2);
+  }
+
+  int computeSumOfSquaresTo(int input) {
+    return IntStream.rangeClosed(1, input).map(i -> (int) Math.pow(i, 2)).sum();
+  }
+
+  int computeDifferenceOfSquares(int input) {
+    return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+  }
+}
